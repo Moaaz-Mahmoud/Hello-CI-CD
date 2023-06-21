fe0871711e5d1ed7e79062b9cbb22159e8c98b3d
Moaaz-Mahmoud <fasekhs@gmail.com>
2023-06-21 13:40:32 +0300

    Add a test that will fail
    Add a test that will fail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def add_numbers(a, b):
+    if a == 8 and b == 2:
+    	return 11
     return a + b
 
+
 result = add_numbers(5, 7)
-print(f'result: {result}')
-print('a')
+print(result)
--- test_main.py
+++ test_main.py
@@ -1,10 +1,16 @@
 import unittest
 from main import add_numbers
 
+
 class TestMain(unittest.TestCase):
-    def test_add_numbers(self):
+    def test_add_numbers_c1(self):
         result = add_numbers(5, 7)
         self.assertEqual(result, 12)
+        
+    def test_add_numbers_c2(self):
+    	result = add_numbers(8, 2)
+    	self.assertEqual(result, 10)
+
 
 if __name__ == '__main__':
     unittest.main()
